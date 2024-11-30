1f46e8e68124f3b56fe068f4f7815678e22d7a52
Yabsira Yetwale <119517823+YabsiraYetwale@users.noreply.github.com>
2024-11-30 17:11:58 +0300

    Update Resume.jsx

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -11,32 +11,7 @@ const Resume = () => {
       </header>
 
       
-      <div className="timeline">
-        <div className="title-wrapper">
-          <div className="icon-box">
-            <FaRegBookmark />
-          </div>
-          <h3 className="h3">Experience</h3>
-        </div>
-        <ol className="timeline-list">
-          <TimelineItem
-            title="Creative director"
-            date="2015 — Present"
-            description="Nemo enim ipsam voluptatem blanditiis praesentium voluptum delenit atque corrupti, quos dolores et qvuas molestias exceptur."
-          />
-          <TimelineItem
-            title="Art director"
-            date="2013 — 2015"
-            description="Nemo enims ipsam voluptatem, blanditiis praesentium voluptum delenit atque corrupti, quos dolores et quas molestias exceptur."
-          />
-          <TimelineItem
-            title="Web designer"
-            date="2010 — 2013"
-            description="Nemo enims ipsam voluptatem, blanditiis praesentium voluptum delenit atque corrupti, quos dolores et quas molestias exceptur."
-          />
-        </ol>
-      </div>
-
+      
       <div className="skill">
         <h3 className="h3 skills-title">My skills</h3>
         <ul className="skills-list content-card">
